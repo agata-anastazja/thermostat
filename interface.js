0c64207299b4ed369ab7e7477744076fdf6316fa
Agata Sumowska <agata.sumowska@gmail.com>
2016-12-09 13:41:14 +0000

    wip

---
+++ interface.js
@@ -0,0 +1,55 @@
+$( document ).ready(function() {
+    // $( "a" ).addClass( "test" );
+    //
+    // alert( "welcome" );
+    //
+    // $( "a" ).click(function( event ) {
+    //
+    //
+    //     event.preventDefault();
+    //     $(this).hide();
+    //
+    //     $.get(index.html, myCallBack);
+    // });
+
+    var thermostat = new Thermostat;
+
+    updateTemperature();
+
+
+    function updateTemperature() {
+        $('#temperature').text(thermostat.temperature);
+    };
+
+
+    $('#increase').click(function() {
+          thermostat.increase();
+          updateTemperature();
+    });
+
+    $('#decrease').click(function() {
+          thermostat.decrease();
+          updateTemperature();
+    });
+
+    $('#reset').click(function() {
+          thermostat.reset();
+          updateTemperature();
+    });
+
+
+    $('#change-mode').click(function() {
+          thermostat.changePowerSaveMode();
+          updateTemperature();
+    });
+
+
+
+
+
+
+
+
+
+
+});
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,34 +1,48 @@
 var Thermostat = function() {
   this.temperature = 20;
-  this._minTemperature = 10;
-  this._maxTemperature = 25;
+  this.MIN_TEMPERATURE = 10;
+  this.MAX_TEMPERATURE_PSM_ON= 25;
+  this.MAX_TEMPERATURE_PSM_OFF= 32;
   this._isPowerSaveOn = true;
 
 };
 
+Thermostat.prototype.getCurrentTemperature = function() {
+  return this.temperature;
+};
+
 
 Thermostat.prototype.increase = function() {
-  this.temperature += 1;
+  if(  this._isPowerSaveOn == true && this.temperature === this.MAX_TEMPERATURE_PSM_ON) {
+    return ;
+    }
+  else if (  this._isPowerSaveOn == false && this.temperature === this.MAX_TEMPERATURE_PSM_OFF) {
+    return ;
+    }
+  else
+  { return this.temperature += 1;}
 
 };
 
 Thermostat.prototype.decrease = function() {
-  this.temperature -= 1;
+  if(this.temperature > this.MIN_TEMPERATURE)
+  { return this.temperature -= 1;}
 
 };
 
 Thermostat.prototype.changePowerSaveMode = function() {
   if(this._isPowerSaveOn = true) {
     this._isPowerSaveOn = false;
-      this._maxTemperature = 32;
   } else {
     this._isPowerSaveOn = true;
-    this._maxTemperature = 25;}
+  }
 };
+
+
+
 
 Thermostat.prototype.reset = function() {
   this.temperature = 20;
-
 };
 
 Thermostat.prototype.energyUsage = function() {
