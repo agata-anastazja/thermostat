e5361c89b1a6ee706e9cebb7503466ffd760e51b
Agata Sumowska <agata.sumowska@gmail.com>
2016-12-09 09:56:49 +0000

    adds a bussiness logic file

---
+++ app.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+    // $( "a" ).addClass( "test" );
+    //
+    // alert( "welcome" );
+    //
+    // $( "a" ).click(function( event ) {
+    //
+    //
+    //     event.preventDefault();
+    //     $(this).hide();
+    //
+    //     $.get(index.html, myCallBack);
+    // });
+
+
+    $("#up").addClass( "test" );
+
+});
